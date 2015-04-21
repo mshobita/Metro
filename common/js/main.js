28af222c0b764985592dff080c30d3255f05cdfd
inas404 <inashazem404@gmail.com>
2015-04-22 01:09:19 +0200

    first

---
+++ common/js/main.js
@@ -0,0 +1,72 @@
+function wlCommonInit(){
+
+	/*
+	 * Application is started in offline mode as defined by a connectOnStartup property in initOptions.js file.
+	 * In order to begin communicating with Worklight Server you need to either:
+	 * 
+	 * 1. Change connectOnStartup property in initOptions.js to true. 
+	 *    This will make Worklight framework automatically attempt to connect to Worklight Server as a part of application start-up.
+	 *    Keep in mind - this may increase application start-up time.
+	 *    
+	 * 2. Use WL.Client.connect() API once connectivity to a Worklight Server is required. 
+	 *    This API needs to be called only once, before any other WL.Client methods that communicate with the Worklight Server.
+	 *    Don't forget to specify and implement onSuccess and onFailure callback functions for WL.Client.connect(), e.g:
+	 *    
+	 *    WL.Client.connect({
+	 *    		onSuccess: onConnectSuccess,
+	 *    		onFailure: onConnectFailure
+	 *    });
+	 *     
+	 */
+	
+	// Common initialization code goes here
+
+}
+function getval(sel) {
+	get_nearest_station
+	alert(sel);
+    var e = document.getElementById("mytheme");
+	var value = element.options[e.selectedIndex].value;
+	var text = element.options[e.selectedIndex].text;
+	alert(value);
+    
+ }
+function change_theme(){
+	var e = document.getElementById("mytheme");
+	var value = element.options[e.selectedIndex].value;
+	var text = element.options[e.selectedIndex].text;
+	alert(text);
+}
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
+function initialize() {
+	alert("hey");
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+  var mapOptions = {
+    zoom:7,
+    center: chicago
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  directionsDisplay.setMap(map);
+}
+
+function calcRoute() {
+	alert("hey2");
+  var start = document.getElementById('start').value;
+  var end = document.getElementById('end').value;
+  var request = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
